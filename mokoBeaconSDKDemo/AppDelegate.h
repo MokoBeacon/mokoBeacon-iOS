0442d1773f989d7b1a0b7c7285abedfb2421e457
Chengang <chengang@mokotechnology.com>
2018-09-05 15:01:03 +0800

    Initial Commit

---
+++ mokoBeaconSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mokoBeaconSDKDemo
+//
+//  Created by aa on 2018/9/5.
+//  Copyright © 2018年 MK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
