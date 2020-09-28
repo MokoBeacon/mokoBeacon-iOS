01966020bf44342da488fd750d8bc6d426e1b2eb
chengang <aadyx2007@163.com>
2020-09-28 16:46:01 +0800

    Fix
    Signed-off-by: chengang <aadyx2007@163.com>

--- README.md
+++
@@ -1,3 +0,0 @@
-mokoBeaconSDK
-#import "HCKBeaconSDK.h"
-
--- mokoBeaconSDK/HCKBeaconSDK.h
+++
@@ -1,5 +0,0 @@
-
-#import "HCKBeaconCentralManager.h"
-#import "HCKBeaconInterface.h"
-#import "HCKBeaconBaseModel.h"
-#import "HCKBeaconProtocol.h"
